3dc2a9464fe51fc4c59cfaf6779c6156eccbeb5a
T Schway <tony.schwebach@gmail.com>
2021-01-08 16:18:32 -0500

    added array of questions

--- index.js
+++ index.js
@@ -1,7 +1,62 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+// const utils = require("utils");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  { name: "title", type: "input", message: "Project Title: " },
+
+  { name: "description", type: "input", message: "Description of project: " },
+
+  {
+    name: "imgPath",
+    type: "input",
+    message: "Relative file path of screensnip/gif: ",
+  },
+
+  {
+    name: "githubUsername",
+    type: "input",
+    message: "GitHub username for contact/questions: ",
+  },
+
+  {
+    name: "contactEmail",
+    type: "input",
+    message: "Email address for contact/questions: ",
+  },
+
+  { name: "deployedURL", type: "input", message: "Deployed site URL: " },
+
+  { name: "repoURL", type: "input", message: "GitHub repo URL: " },
+
+  {
+    name: "installation",
+    type: "input",
+    message: "Describe installation instructions: ",
+  },
+
+  {
+    name: "usage",
+    type: "input",
+    message: "Describe usage instructions: ",
+  },
+
+  {
+    name: "contribution",
+    type: "input",
+    message: "Describe contribution instructions: ",
+  },
+
+  {
+    name: "testing",
+    type: "input",
+    message: "Describe testing instructions: ",
+  },
+];
+
+//licence
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -9,6 +64,8 @@ function writeToFile(fileName, data) {}
 // TODO: Create a function to initialize app
 function init() {
   console.log("hello world");
+  inquirer.prompt(questions)
+  .then((response)=>console.log(response));
 }
 
 // Function call to initialize app
